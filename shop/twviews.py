982d74f509fc7cacff7528fd7f01afb4f61effb8
Alexander Emchinov <aleksandr.emchinov@phystech.edu>
2018-05-02 23:04:53 +0300

    Add ability to add goods from user interface

--- shop/twviews.py
+++ shop/twviews.py
@@ -2,6 +2,16 @@ from django.http import  Http404
 from django.views.generic.base import TemplateView
 from django.core.paginator import Paginator, InvalidPage
 from shop.models import Category, Good
+from django.views.generic.base import ContextMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import ProcessFormView
+from django.urls import reverse
+
+class CategoryListMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListMixin, self).get_context_data(**kwargs)
+        context["cats"] = Category.objects.order_by("name")
+        return context
 
 class GoodListView(TemplateView):
     template_name = "index.html"
@@ -34,3 +44,49 @@ class GoodDetailView(TemplateView):
             raise Http404("Вы кто такие? Я вас не звал.")
         context["cats"] = Category.objects.order_by("name")
         return context
+
+
+class GoodEditMixin(CategoryListMixin):
+    def get_context_data(self, **kwargs):
+        context = super(GoodEditMixin, self).get_context_data(**kwargs)
+        try:
+            context["pn"] = self.request.GET["page"]
+        except KeyError:
+            context["pn"] = 1
+        return context
+
+class GoodEditView(ProcessFormView):
+    def post(self, request, *args, **kwargs):
+        try:
+            pn = request.GET["page"]
+        except KeyError:
+            pn = "1"
+        self.success_url = self.success_url + "?page" + pn
+        return super(GoodEditView, self).post(request, *args, **kwargs)
+
+class GoodCreate(CreateView, GoodEditMixin):
+    model = Good
+    fields = '__all__'
+    template_name = "good_add.html"
+    def get(self, request, *args, **kwargs):
+        if self.kwargs["cat_id"] != None:
+            self.initial["category"] = Category.objects.get(pk=self.kwargs["cat_id"])
+        return super(GoodCreate, self).get(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        self.success_url = reverse("index", kwargs={"cat_id": Category.objects.get(pk=self.kwargs["cat_id"]).id})
+        return super(GoodCreate, self).post(request, *args, **kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(GoodCreate, self).get_context_data(**kwargs)
+        context["category"] = Category.objects.get(pk=self.kwargs["cat_id"])
+        return context
+
+
+
+
+
+
+
+
+
+
+
--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.conf.urls import url
 from shop import views
-from shop.twviews import GoodListView, GoodDetailView
+from shop.twviews import GoodListView, GoodDetailView, GoodCreate
 """
 urlpatterns = [
     #url(r'^(?: (?P<id>\d+)/)?$', views.index, name="index"),
@@ -14,5 +14,6 @@ urlpatterns = [
     #url(r'^(?: (?P<id>\d+)/)?$', views.index, name="index"),
     url(r'^(?P<cat_id>\d+)/$', GoodListView.as_view(), name="index"),
     url(r'^good/(?P<id>\d+)/$', GoodDetailView.as_view(), name="good"),
+    url(r'^(?P<cat_id>\d+)/add/$', GoodCreate.as_view(), name="good_add"),
     #url(r'', views.list, name="list"),
 ]
